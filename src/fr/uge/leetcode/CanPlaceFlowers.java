030bd885009e2876a254e068f5684146d461287d
Roland <roland.zhang.pro@gmail.com>
2026-01-23 09:59:09 +0100

    leetcode 605

--- src/fr/uge/leetcode/CanPlaceFlowers.java
+++ src/fr/uge/leetcode/CanPlaceFlowers.java
@@ -2,6 +2,17 @@ package fr.uge.leetcode;
 
 public class CanPlaceFlowers {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        return false;
+        for(var i = 0; i < flowerbed.length; i++){
+            if(n <= 0) return true;
+            if(flowerbed[i] == 0){
+                var left = (i == 0) || (flowerbed[i - 1] == 0);
+                var right = (i == flowerbed.length - 1) || (flowerbed[i + 1] == 0);
+                if(left && right){
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+        }
+        return n <= 0;
     }
 }
