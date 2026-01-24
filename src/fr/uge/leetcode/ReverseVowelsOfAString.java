3abad6c33bffbbb667c6795b847c4ba8b57e6b8c
Roland <roland.zhang.pro@gmail.com>
2026-01-24 23:21:40 +0100

    create leetcode 345

---
+++ src/fr/uge/leetcode/ReverseVowelsOfAString.java
@@ -0,0 +1,10 @@
+package fr.uge.leetcode;
+
+public class ReverseVowelsOfAString {
+    public String reverseVowels(String s) {
+        for(var i = 0; i < s.length(); i++){
+            var letter = s.charAt(i);
+        }
+        return "";
+    }
+}
