ad9c14eba770f38b025f7f7da611430389e5c2df
Roland <roland.zhang.pro@gmail.com>
2026-01-28 18:34:12 +0100

    update leetcode 151

--- src/fr/uge/leetcode/ReverseWordsInAString.java
+++ src/fr/uge/leetcode/ReverseWordsInAString.java
@@ -8,6 +8,8 @@ public class ReverseWordsInAString {
     public String reverseWords(String s) {
         var words = s.split(" ");
         Collections.reverse(Arrays.asList(words));
-        return Arrays.stream(words).filter(word -> !word.isEmpty()).collect(Collectors.joining(" "));
+        return Arrays.stream(words)
+                .filter(word -> !word.isEmpty())
+                .collect(Collectors.joining(" "));
     }
 }
